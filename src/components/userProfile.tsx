a5d8fc4fe6168e51e5c7b5e42b31addd9d242b53
surafelhabte1@gmail.com <surafelhabte1@gmail.com>
2025-07-04 16:28:02 +0300

    feat: update UserProfile component to use Link for navigation and improve styling

--- src/components/userProfile.tsx
+++ src/components/userProfile.tsx
@@ -1,24 +1,30 @@
 import { Avatar } from "antd";
 import { UserOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
 const UserProfile = ({
+  _id,
   firstName,
   lastName,
   photoUrl,
 }: {
+  _id?: string;
   firstName: string;
   lastName: string;
   photoUrl: string;
 }) => {
   return (
-    <div className='flex items-center'>
+    <Link
+      className='flex items-center hover:underline hover:cursor-pointer !text-black'
+      to={`/users/${_id ? _id : `${firstName}-${lastName}`}`}
+    >
       {photoUrl ? (
         <Avatar src={photoUrl} size={40} style={{ marginRight: 8 }} />
       ) : (
         <Avatar icon={<UserOutlined />} size={40} style={{ marginRight: 8 }} />
       )}
-      <span className='ml-3 text-nowrap'>{`${firstName} ${lastName}`}</span>
-    </div>
+      <span className='ml-3 font-light text-nowrap'>{`${firstName} ${lastName}`}</span>
+    </Link>
   );
 };
 
