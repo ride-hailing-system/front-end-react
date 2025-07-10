35ec84e8257e27746564906baa53d8dbba3a5ce0
surafelhabte1@gmail.com <surafelhabte1@gmail.com>
2025-07-10 19:10:23 +0300

    feat: remove unused Drivers, Riders, EntryForm, and List components

--- src/pages/drivers.tsx
+++
@@ -1,9 +0,0 @@
-const Drivers = () => {
-  return (
-    <p className='flex justify-center items-center text-lg font-bold text-gray-600'>
-      Drivers
-    </p>
-  );
-};
-
-export default Drivers;
--- src/pages/riders.tsx
+++
@@ -1,9 +0,0 @@
-const Riders = () => {
-  return (
-    <p className='flex justify-center items-center text-lg font-bold text-gray-600'>
-      Riders
-    </p>
-  );
-};
-
-export default Riders;
