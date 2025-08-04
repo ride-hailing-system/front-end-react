2403ad6615cf6e6a904fe464ae04e2488a1f1e49
surafelhabte1@gmail.com <surafelhabte1@gmail.com>
2025-08-04 14:20:15 +0300

    wrap loading spinner with LoadScript for Google Maps integration

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,20 @@ import Routes from "./routes";
 import { useContext } from "react";
 import { Spin } from "antd";
 import { LoadingContext } from "./context/loadingContext";
+import { LoadScript } from "@react-google-maps/api";
+import { MAP_API_KEY } from "./utils/globalVariables";
 
 const App = () => {
   const { loadingData } = useContext(LoadingContext);
 
   return (
     <>
-      <Spin spinning={loadingData?.isLoading}>
-        <ConfirmModal />
-        <Routes />;
-      </Spin>
+      <LoadScript googleMapsApiKey={MAP_API_KEY} libraries={["places"]}>
+        <Spin spinning={loadingData?.isLoading}>
+          <ConfirmModal />
+          <Routes />
+        </Spin>
+      </LoadScript>
     </>
   );
 };
