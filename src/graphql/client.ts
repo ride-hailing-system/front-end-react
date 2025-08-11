ed25e919f8b8e761192d600ffddbcc65b1797a5b
surafelhabte1@gmail.com <surafelhabte1@gmail.com>
2025-08-11 18:23:49 +0300

    update the backend endpoint

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,14 +1,15 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+const uri: string = import.meta.env.DEV
+    ? 'http://localhost:4000/graphql'
+    : 'https://ride-hailing-backend-mcsickdfy-surafels-projects.vercel.app/graphql'
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:4000/graphql', 
+  uri: uri, 
   credentials: 'include',
 });
 
 export const client = new ApolloClient({
-  uri: import.meta.env.DEV
-    ? 'http://localhost:4000/graphql'
-    : 'http://localhost:4000/graph',
+  uri: uri,
   link: httpLink,
   cache: new InMemoryCache({
     addTypename: false,
